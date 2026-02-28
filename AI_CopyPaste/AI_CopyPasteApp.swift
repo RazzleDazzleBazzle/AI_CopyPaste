d576301895857ff885c4490f769c664c404b1d57
Darren Wilson <darrenwilson23@icloud.com>
2026-02-28 18:18:19 +1100

    Initial Commit

---
+++ AI_CopyPaste/AI_CopyPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_CopyPasteApp.swift
+//  AI_CopyPaste
+//
+//  Created by Darren Wilson on 28/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_CopyPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI_CopyPaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI_CopyPaste
+//
+//  Created by Darren Wilson on 28/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
